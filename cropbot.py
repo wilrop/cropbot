6a43714fd71d2917551ee4063d5864984a274362
Willem Röpke <willem.ropke@vub.be>
2022-09-07 09:30:04 +0200

    Implement bayesian model of environment

--- cropbot.py
+++ cropbot.py
@@ -14,11 +14,13 @@ class CropBot:
     A class representing the CropBot.
     """
 
-    def __init__(self, width, height, weights, start=(0, 0), prior_sick=0.1, prior_conditional_sick=0.5):
+    def __init__(self, width, height, weights, start=(0, 0), prior_risk=0.1, prior_conditional_risk=0.5,
+                 empirical_threshold=10):
         self.width = width
         self.height = height
         self.weights = self.normalise_weights(weights)
         self.state = start
+        self.prev = None
         self.plan = []
 
         self.serial_port = '/dev/ttys002'
@@ -26,17 +28,19 @@ class CropBot:
         self.directions = {(0, 0): 'stay', (0, 1): 'forward', (0, -1): 'backward', (-1, 0): 'left', (1, 0): 'right'}
         self.driver_delay = 15
 
-        self.unchecked = np.zeros((width, height))
-        self.inverse_distances = self.calc_inverse_distances(width, height)
-        self.current_risk = np.zeros((width, height))
-        self.risk_model = np.zeros((width, height, 2))
-        self.prior_sick = prior_sick
-        self.prior_conditional_sick = prior_conditional_sick
-
         self.graph = nx.grid_2d_graph(width, height).to_directed()
         self.edge_values = defaultdict(int)
         self.vertex_values = defaultdict(int)
         self.risk_values = defaultdict(int)
+
+        self.unchecked = np.zeros((width, height))
+        self.inverse_distances = self.calc_inverse_distances(width, height)
+        self.active_interventions = np.zeros((width, height))
+        self.intervention_freqs = np.zeros((width, height, 2))
+        self.conditional_freqs = self.setup_conditional_freqs()
+        self.empirical_threshold = empirical_threshold
+        self.prior_risk = prior_risk
+        self.prior_conditional_risk = prior_conditional_risk
 
         self.log_world_state()
 
@@ -87,6 +91,19 @@ class CropBot:
                 distances[w1, h1, w2, h2] = max_distance - distance
         return distances
 
+    def setup_conditional_freqs(self):
+        """Build the dictionary of conditional frequencies.
+
+        Returns:
+            Dict: A dictionary of conditional intervention frequencies of vertices and neighbours of them.
+        """
+        conditional_freqs = {}
+        for vertex in self.graph:
+            conditional_freqs[vertex] = {}
+            for neighbour in self.graph.neighors(vertex):
+                conditional_freqs[vertex][neighbour] = np.zeros(2)
+        return conditional_freqs
+
     def get_weight_for_edge(self, start_vertex, end_vertex):
         """Get the weight for a given edge.
 
@@ -105,20 +122,65 @@ class CropBot:
     def normalise_scalar(x, min_x, max_x):
         return x if x == 0 else (x - min_x) / (max_x - min_x)
 
-    def compute_intervention_freq(self, vertex):
-        """Compute the intervention frequency for a given vertex.
-
-        Args:
-            vertex (Tuple[int]): The coordinates of the vertex.
+    def get_active_neighbours(self, vertex):
+        """Get the neighbours who have active interventions.
 
         Returns:
-            float: The intervention frequency.
+            List[Tuple[int]]: A tuple of active neighbours.
         """
-        if self.risk_model[vertex][0] == 0:
-            intervention_freq = 0
+        active_neighbours = []
+
+        for neighbour in self.graph.neighbors(vertex):
+            if self.active_interventions[neighbour]:
+                active_neighbours.append(neighbour)
+        return active_neighbours
+
+    def compute_prob(self, interventions, total):
+        """Compute the empirical probability for an intervention over a total if the total exceeds the threshold.
+
+        Args:
+            interventions (int): The number of interventions.
+            total (int): The total number of samples.
+
+        Returns:
+            float: A probability estimate.
+        """
+        if total >= self.empirical_threshold:
+            risk_prob = interventions / total
         else:
-            intervention_freq = self.risk_model[vertex][0] / self.risk_model[vertex][1]
-        return intervention_freq
+            risk_prob = self.prior_risk
+        return risk_prob
+
+    def compute_risk_prob(self, vertex):
+        """Compute the current risk estimate for a given vertex.
+
+        Args:
+            vertex (Tuple[int]): A given vertex.
+
+        Returns:
+            float: A risk estimate for this vertex as a probability.
+        """
+        interventions, total = self.intervention_freqs[vertex]
+        risk_prob = self.compute_prob(interventions, total)
+        active_neighbours = self.get_active_neighbours(vertex)
+
+        if active_neighbours:
+            neighbours_probs = []
+            conditional_neighbours_probs = []
+
+            for neighbour in active_neighbours:
+                interventions, total = self.intervention_freqs[neighbour]
+                conditional_interventions, conditional_total = self.conditional_freqs[neighbour][vertex]
+                neighbours_probs.append(self.compute_prob(interventions, total))
+                conditional_neighbours_probs.append(self.compute_prob(conditional_interventions, conditional_total))
+
+            neighbours_risk_prob = np.prod(neighbours_probs)
+            conditional_risk_prob = np.prod(conditional_neighbours_probs)
+
+            conditional_risk = conditional_risk_prob * risk_prob / neighbours_risk_prob
+            return conditional_risk
+        else:
+            return risk_prob
 
     def update_vertex_values(self):
         """Update the values for the vertices in the graph.
@@ -134,23 +196,13 @@ class CropBot:
         min_inv_dist = np.min(self.inverse_distances)
         max_inv_dist = np.max(self.inverse_distances)
 
-        risk_values = []
         for vertex in self.graph.nodes:
-            intervention_freq = self.compute_intervention_freq(vertex)
-            risk_value = self.current_risk[vertex] * (1 + intervention_freq)
-            risk_values.append(risk_value)
-
-        min_intervention_value = min(risk_values)
-        max_intervention_value = max(risk_values)
-
-        for vertex, risk_value in zip(self.graph.nodes, risk_values):
             unchecked_value = self.normalise_scalar(self.unchecked[vertex], min_unchecked, max_unchecked)
             distance_value = self.normalise_scalar(self.inverse_distances[self.state + vertex], min_inv_dist,
                                                    max_inv_dist)
-            risk_value = self.normalise_scalar(risk_value, min_intervention_value, max_intervention_value)
+            risk_value = self.risk_values[vertex]  # Is already a probability.
             vertex_value = self.weights[0] * unchecked_value + self.weights[1] * distance_value + self.weights[
                 2] * risk_value
-            self.risk_values[vertex] = risk_value
             self.vertex_values[vertex] = vertex_value
 
     def update_edge_weights(self):
@@ -172,17 +224,6 @@ class CropBot:
         self.unchecked += 1
         self.unchecked[self.state] = 0
 
-    def update_area(self):
-        """Update the values of an area around an intervention."""
-        min_x = max(0, self.state[0] - 1)
-        max_x = min(self.width - 1, self.state[0] + 1)
-        min_y = max(0, self.state[1] - 1)
-        max_y = min(self.height - 1, self.state[1] + 1)
-
-        for x in range(min_x, max_x + 1):
-            for y in range(min_y, max_y + 1):
-                self.current_risk[x, y] += 1
-
     @staticmethod
     def classify_color(color):
         """Classify an observed color by the driver.
@@ -202,26 +243,29 @@ class CropBot:
         else:
             return 'normal'
 
-    def update_interventions(self, res_dict):
-        """Update the value of a cell when an intervention should take place.
-
-        Note: Handle the interventions differently
+    def update_model(self, res_dict):
+        """Update the statistical model of the environment.
 
         Args:
-            res_dict (Dict): The response from the driver with their observation.
+            res_dict (Dict): A dictionary containing the result of the driver.
         """
         color = res_dict['color']
         classification = self.classify_color(color)
-        if classification == 'diseased':
-            self.update_area()
-        elif classification == 'pests':
-            self.update_area()
-        elif classification == 'flooding':
-            self.update_area()
-        elif classification == 'drought':
-            self.update_area()
+        intervention_necessary = classification in ['diseased', 'pests', 'flooding', 'drought']
+
+        if self.active_interventions[self.prev]:
+            self.conditional_freqs[self.state][self.prev][0] += intervention_necessary
+            self.conditional_freqs[self.state][self.prev][1] += 1
+
+        if intervention_necessary:
+            self.active_interventions[self.state] = 1
+            self.intervention_freqs[self.state][0] += 1
+            self.conditional_freqs[self.prev][self.state][0] += self.active_interventions[self.prev]
+            self.conditional_freqs[self.prev][self.state][1] += 1
         else:
-            self.current_risk[self.state] = 0
+            self.active_interventions[self.state] = 0
+
+        self.intervention_freqs[self.state][1] += 1
 
     def get_plan(self, target):
         """Compute the optimal trajectory from the current state to a given target.
@@ -362,9 +406,11 @@ class CropBot:
             res_dict = self.command_driver(direction)
 
             if res_dict['success']:
+                self.prev = self.state
                 self.state = next_state
+                self.update_model(res_dict)
                 self.update_unchecked()
-                self.update_interventions(res_dict)
+                self.risk_values = {vertex: self.compute_risk_prob(vertex) for vertex in self.graph}
                 self.log_world_state()
             else:
                 return False
